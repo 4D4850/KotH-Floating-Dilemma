747c5bbc863dc7edc612af3219787d07fc7eb4bb
4D4850 <84929757+4D4850@users.noreply.github.com>
2021-06-10 12:53:46 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -5,16 +5,17 @@ class Controller:
     self.points = [0, 0]
     self.plan = plan
 
-  def play(self)
-    a = self.strategy[0]
-    b = self.strategy[1]
+  def play(self, is_new_game):
+    a = self.strategy[0](is_new_game)
+    b = self.strategy[1](is_new_game)
     self.points[0] += a * b * 2 + (1 - a) * b * 3 + (1 - a) * (1 - b)
     self.points[1] += a * b * 2 + a * (1 - b) * 3 + (1 - a) * (1 - b)
     self.plan[0](b)
     self.plan[1](a)
   def run(self, numgames):
+    self.play(1)
     for _  in range(numgames):
-      self.play()
+      self.play(0)
 
     print("Results:" + self.points)
     return self.points
